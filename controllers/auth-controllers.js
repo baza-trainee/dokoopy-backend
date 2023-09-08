7e92c33f7fa44d4c34964147f33fa0ca6207e97a
Olena Stasiuk <estasjuk@gmail.com>
2023-09-08 19:02:52 +0300

    add feature/forgot-reset-password

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -4,6 +4,9 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const HttpError = require("../utils/HttpError");
 const controllerWrapper = require("../utils/controllerWrapper");
+const { RESET_PASSWORD_SECRET_KEY, BASE_URL, FRONT_LOCALHOST } = process.env;
+const resetPasswordHtml = require('../utils/resetPasswordEmail');
+const sendEmail = require('../utils/sendEmail');
 
 const userPath = path.join(__dirname, "../db/users/users.json");
 
@@ -58,8 +61,71 @@ const getCurrentUser = async (req, res) => {
   res.status(200).json(answer);
 };
 
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  const usersData = await fs.readFile(userPath, "utf-8");
+  const user = await JSON.parse(usersData);
+
+  if (user.email !== email) {
+    throw new HttpError(404, "User not found")
+  }
+
+  const payload = {
+      email: user.email,
+  };
+
+  const resetToken = jwt.sign(payload, RESET_PASSWORD_SECRET_KEY, { expiresIn: 3600 });
+
+  const resetPasswordEmail = {
+      to: email,
+      subject: "Зміна паролю для входу на сайт Dokoopy",
+      html: `${resetPasswordHtml}
+      target="_blank" href="${BASE_URL}/api/auth/reset-password/${resetToken}">Змінити пароль</a>
+      </div>
+      `
+  };
+
+  await sendEmail(resetPasswordEmail);
+
+  res.status(200).json({
+      status: 'success',
+      code: 200,
+      message: "Reset password email sent"
+  })
+};
+
+const resetPassword = async (req, res) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
+
+  const usersData = await fs.readFile(userPath, "utf-8");
+  const user = await JSON.parse(usersData);
+
+  jwt.verify(resetToken, RESET_PASSWORD_SECRET_KEY, function(err, decoded) {
+      if (err) {
+          throw new HttpError(403, "Reset token is expired")
+      }
+    });
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  user.password = hashPassword;
+  await fs.writeFile(userPath, JSON.stringify(user), "utf-8");
+
+  res.status(200).json({
+      status: 'success',
+      code: 200,
+      message: "Reset password is succesful"
+  })
+};
+
 module.exports = {
   login: controllerWrapper(login),
   logout: controllerWrapper(logout),
   getCurrentUser: controllerWrapper(getCurrentUser),
+  forgotPassword: controllerWrapper(forgotPassword),
+  resetPassword: controllerWrapper(resetPassword),
 };
+
+
+  //"email": "estasjuk@gmail.com",
+  //"password": "qwe123"
---
+++ utils/resetPasswordEmail.js
@@ -0,0 +1,21 @@
+const resetPasswordHtml = `<div style = 
+"width: 640px;
+padding: 8px;">
+<p style=
+"font-family: 'Roboto', sans-serif;
+font-weight: 500;
+font-size: 14px;
+line-height: 1.14;
+letter-spacing: 0.02em;
+color: black;
+">
+Для зміни паролю перейдіть, будь ласка, за посиланням:</p>
+<a style="
+font-family: 'Roboto', sans-serif;
+font-weight: 700;
+font-size: 20px;
+line-height: 1.14;
+letter-spacing: 0.02em;
+"`;
+
+module.exports = resetPasswordHtml;
