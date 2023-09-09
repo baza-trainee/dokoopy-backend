eb2200f398aedf0c5601e55cc813415fa46ddeca
Olena Stasiuk <estasjuk@gmail.com>
2023-09-09 19:03:56 +0300

    add feature/bank

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const swaggerDocument = require("./swagger.json");
 const mainRouter = require("./routes/api/main/main-routes");
 const authRouter = require("./routes/api/auth/auth-routes");
 const projectsRouter = require("./routes/api/projects/projects-routes");
+const bankRouter = require("./routes/api/bank/bank-routes");
 
 const app = express();
 
@@ -31,8 +32,8 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRouter);
 app.use("/api/main", mainRouter);
-app.use("/api/auth", authRouter);
 app.use("/api/projects", projectsRouter);
+app.use("/api/bank", bankRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ controllers/bank-controllers.js
@@ -0,0 +1,31 @@
+const fs = require('fs/promises');
+require("dotenv").config();
+const { nanoid } = require('nanoid');
+const path = require('path');
+const controllerWrapper = require("../utils/controllerWrapper");
+
+
+const bankPath = path.join(__dirname, '../db/bank/bank.json');
+
+const updateBank = async(req, res) => {
+    const bank_data = await fs.readFile(bankPath, 'utf-8');
+    const bank = JSON.parse(bank_data);
+
+    const updBank = {
+        id: req.params.id,
+        link: req.body.link,
+    }
+
+    bank.splice(0, 1, updBank);
+    await fs.writeFile(bankPath, JSON.stringify(bank, null, 2));
+
+        res.status(200).json({
+            status: 'success',
+            code: 200,
+            updBank,
+    });
+};
+
+module.exports = {
+    updateBank: controllerWrapper(updateBank),
+};
